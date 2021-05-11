e886f795bfda646d3bbd5e4c5230b6172e5d7efe
Eugene A. Kuchin <kea@project-osnova.ru>
2021-05-11 14:17:50 +0500

    First Demo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <v-card class="ma-5 pa-5">
+      <v-card-title>Имя кота</v-card-title>
+      <v-card-text>{{ catname }}</v-card-text>
+      <v-card-actions>
+        <v-btn>Погладить</v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card class="ma-5 pa-5">
+      <v-text-field label="Имя кота" type="text" v-model="catname" />
+    </v-card>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => {
+    return { catname: "Мурзик" };
+  },
+};
+</script>
